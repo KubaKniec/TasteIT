c10566a098e9dbd08e73e80f3a7d51b7a7998197
JakubKonkol <s24406@pjwstk.edu.pl>
2023-10-24 11:12:40 +0200

    fetcher

--- MixITServer/src/main/java/mixitserver/api/Fetcher.java
+++ MixITServer/src/main/java/mixitserver/api/Fetcher.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Fetcher {
+    public String URL = "www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+    void fetchDrinkById(int id){
+        String url = this.URL+id;
+
+    }
 }

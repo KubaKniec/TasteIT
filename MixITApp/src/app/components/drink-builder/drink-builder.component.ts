c17012e090347d75762c0b369542d374ea129650
JakubKonkol <s24406@pjwstk.edu.pl>
2023-11-11 20:53:48 +0100

    DrinkBuilder pt 2

--- MixITApp/src/app/components/drink-builder/drink-builder.component.ts
+++ MixITApp/src/app/components/drink-builder/drink-builder.component.ts
@@ -12,12 +12,30 @@ export class DrinkBuilderComponent implements OnInit{
 
               ){}
   ingredients: String[] = [];
+  filteredIngredients: String[] = [];
+  searchPhrase: String = "";
+  selectedIngredients: String[] = [];
   ngOnInit(): void {
     this.publicIngredientsService.getAllIngredientsNames().then((ingredients) => {
       this.ingredients = ingredients;
+      this.ingredients = this.removeDuplicateIngredients(this.ingredients);
+      this.filteredIngredients = ingredients;
     }).catch((error) => {
       console.log(error);
     });
   }
+  removeDuplicateIngredients(ingredients: String[]): String[] {
+    return ingredients.filter((ingredient, index) => ingredients.indexOf(ingredient) === index);
+  }
+  handleIngredientClick(ingredient: String) {
+    if (this.selectedIngredients.includes(ingredient)) {
+      this.selectedIngredients = this.selectedIngredients.filter((selectedIngredient) => selectedIngredient !== ingredient);
+    } else {
+      this.selectedIngredients.push(ingredient);
+    }
+  }
 
+  filterIngredientsByPhrase() {
+    this.filteredIngredients = this.ingredients.filter((ingredient) => ingredient.toLowerCase().includes(this.searchPhrase.toLowerCase()));
+  }
 }

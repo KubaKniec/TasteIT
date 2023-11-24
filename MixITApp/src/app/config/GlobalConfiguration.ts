3a20d8b9f1509472304031a348ab8cefc376cebf
JakubKonkol <s24406@pjwstk.edu.pl>
2023-11-24 17:42:36 +0100

    envs

--- MixITApp/src/app/config/GlobalConfiguration.ts
+++ MixITApp/src/app/config/GlobalConfiguration.ts
@@ -1,5 +1,7 @@
+import {environment} from "../../environments/environment";
+
 export const GlobalConfiguration = {
-  API_URL: 'http://localhost:8080/',
+  API_URL: environment.API_URL,
   APP_VERSION: '1.0 DevBuild',
   APP_NAME: 'MixIT',
   ALLOW_ALL_DEVICES: true,

6ea29e5e2eecd6d3ccc7c81d4ebed36290794e60
JakubKonkol <s24406@pjwstk.edu.pl>
2023-11-25 18:01:29 +0100

    adult warning fixed + initial install app modal

--- MixITApp/src/app/app.component.ts
+++ MixITApp/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewContainerRef} from '@angular/core';
+import {GlobalConfiguration} from "./config/GlobalConfiguration";
+import {AdultWarningFactoryService} from "./service/AdultWarningFactoryService";
+import {BodyScrollService} from "./service/BodyScrollService";
+import {InstallAppModalFactoryService} from "./service/InstallAppModalFactoryService";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'MixITApp';
+export class AppComponent implements OnInit{
+  title = 'MixIT';
+  constructor(private adultWarningFactoryService: AdultWarningFactoryService,
+              private viewContainerRef: ViewContainerRef,
+              private bodyScrollService: BodyScrollService,
+              private installAppModalFactoryService: InstallAppModalFactoryService
+  ) {
+    this.adultWarningFactoryService.setRootViewContainerRef(this.viewContainerRef);
+    this.installAppModalFactoryService.setRootViewContainerRef(this.viewContainerRef);
+  }
+  ngOnInit(): void {
+    if(GlobalConfiguration.SHOW_WARNINGS){
+      this.bodyScrollService.disableScroll();
+      const componentRef = this.adultWarningFactoryService.addDynamicComponent();
+      componentRef.instance.close.subscribe(() => {
+        this.adultWarningFactoryService.removeDynamicComponent(componentRef)
+        this.bodyScrollService.enableScroll();
+      });
+    }
+    if(!GlobalConfiguration.ALLOW_ALL_DEVICES && !window.matchMedia('(display-mode: standalone)').matches){
+      this.bodyScrollService.disableScroll();
+      const componentRef = this.installAppModalFactoryService.addDynamicComponent();
+      componentRef.instance.close.subscribe(() => {
+        this.installAppModalFactoryService.removeDynamicComponent(componentRef)
+        this.bodyScrollService.enableScroll();
+      });
+    }
+  }
+
 }
--- MixITApp/src/app/components/install-app-modal/install-app-modal.component.ts
+++ MixITApp/src/app/components/install-app-modal/install-app-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-install-app-modal',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./install-app-modal.component.css']
 })
 export class InstallAppModalComponent {
+  @Output() close: EventEmitter<void> = new EventEmitter<void>();
 
 }

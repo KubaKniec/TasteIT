2f6eae88f990c2e6dbd4b68fbaa47981131de2c2
JakubKonkol <s24406@pjwstk.edu.pl>
2023-11-25 12:29:14 +0100

    adult warning

--- MixITApp/src/app/components/adult-warning/adult-warning.component.ts
+++ MixITApp/src/app/components/adult-warning/adult-warning.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-adult-warning',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./adult-warning.component.css']
 })
 export class AdultWarningComponent {
+  confirmInput: string = '';
+  isButtonDisabled: boolean = true;
+  @Output() close: EventEmitter<void> = new EventEmitter<void>();
 
+  checkConfirmation() {
+    if (this.confirmInput === 'I agree') {
+      console.log('Confirmed');
+      this.isButtonDisabled = false;
+    }else{
+      this.isButtonDisabled = true;
+    }
+  }
+  onClose() {
+    this.close.emit();
+  }
 }

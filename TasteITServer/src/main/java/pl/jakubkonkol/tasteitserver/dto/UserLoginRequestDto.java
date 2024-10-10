9f2cbc47ca6deaf14c7534bd8868cabec74e73c1
KubaKniec <jakub.kniec02@gmail.com>
2024-10-10 20:53:07 +0200

    [RC] <-- [187 Better-validation] Done

--- TasteITServer/src/main/java/pl/jakubkonkol/tasteitserver/dto/UserLoginRequestDto.java
+++ TasteITServer/src/main/java/pl/jakubkonkol/tasteitserver/dto/UserLoginRequestDto.java
@@ -9,11 +9,17 @@ public class UserLoginRequestDto {
     @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Provided email has an invalid format")
     private String email;
     @NotBlank(message = "Field password cannot be blank and cannot be null")
-    @Pattern(regexp = "^(?=.*[0-9])"
+    @Pattern(regexp =
+//            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\w\\s])[A-Za-z\\d!\"#$%&'()*+,\\-./:;<=>?@[\\\\\\]^_`{|}~]{8,20}$"
+            "^(?=.*[0-9])"
             + "(?=.*[a-z])(?=.*[A-Z])"
-            + "(?=.*[@#$%!^&+=])"
-            + "(?=\\S+$).{6,20}$", message = "Provided password has an invalid format. " +
+            + "(?=.*[#$%&'()*+,\\-./:;<=>?@\\[\\\\\\]^_`{|}~])"
+            + "(?=\\S+$).{6,20}$"
+            , message = "Provided password has an invalid format. " +
             "Should be: 6-20 characters long, contains at least one digit, one lowercase letter, " +
-            "one uppercase letter, one special character, and has no whitespace")
+            "one uppercase letter, one special character '  ', and has no whitespace")
     private String password;
+
+
 }
+

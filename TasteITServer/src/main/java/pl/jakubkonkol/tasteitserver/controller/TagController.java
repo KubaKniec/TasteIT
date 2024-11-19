4d0932a903235433c417a3b978bf8292f0b60f77
JakubKonkol <s24406@pjwstk.edu.pl>
2024-11-19 11:21:17 +0100

    fixed saveTag

--- TasteITServer/src/main/java/pl/jakubkonkol/tasteitserver/controller/TagController.java
+++ TasteITServer/src/main/java/pl/jakubkonkol/tasteitserver/controller/TagController.java
@@ -2,10 +2,7 @@ package pl.jakubkonkol.tasteitserver.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.jakubkonkol.tasteitserver.model.Tag;
 import pl.jakubkonkol.tasteitserver.service.TagService;
 
@@ -29,7 +26,7 @@ public class TagController {
                 ResponseEntity.ok(tagService.getAll());
     }
     @PostMapping("/")
-    public ResponseEntity<Tag> saveTag(Tag tag){
+    public ResponseEntity<Tag> saveTag(@RequestBody Tag tag){
         return ResponseEntity.ok(tagService.save(tag));
     }
 }
